dc873d07a813e37f19eab85571cd8057f13382ef
Marc Bouvier <m.bouvier.dev@gmail.com>
2025-11-08 15:13:28 +0100

    feat: données d'analyse en local

--- _data/sites/template.js
+++ _data/sites/template.js
@@ -1,10 +1,11 @@
 module.exports = {
 	name: "template",
 	description: "Modèle point de départ des projets",
-	options: {
-		runs: 1,
-		frequency: 1
-	},
+    options: {
+        runs: 1, // Run sample every time
+        frequency: 60 * 24, // Every 1 day
+        freshChrome: "site" // Use "site" if sites are all on the same origin and share assets.
+    },
 	urls: [
 		"https://coda-school.github.io/template-projet-html-groupe/"
 	]
