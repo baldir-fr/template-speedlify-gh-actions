d620edef912e44e2b6bcd0af9a605dd920f8fbdc
Marc Bouvier <m.bouvier.dev@gmail.com>
2025-11-09 21:39:24 +0100

    feat: ajout d'un huitième groupe en cas de petits groupes

---
+++ _data/sites/groupe_8.js
@@ -0,0 +1,12 @@
+module.exports = {
+    name: "25-html-g8",
+    description: "2025 B1 - Projet HTML du groupe 8",
+    options: {
+        runs: 1, // Run sample every time
+        frequency: 30, // Every 30 minutes
+        freshChrome: "site" // Use "site" if sites are all on the same origin and share assets.
+    },
+    urls: [
+        "https://coda-school.github.io/25-html-g8"
+    ]
+};
