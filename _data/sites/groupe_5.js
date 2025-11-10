972f208dcd4edd446dd68ace5bc31c8b8fba4c54
Marc Bouvier <m.bouvier.dev@gmail.com>
2025-11-10 18:46:48 +0100

    feat: nouvelles pages pour le groupe 5

--- _data/sites/groupe_5.js
+++ _data/sites/groupe_5.js
@@ -8,5 +8,9 @@ module.exports = {
     },
     urls: [
         "https://coda-school.github.io/25-html-g5/", // Ne pas oublier trailing slash
+        "https://coda-school.github.io/25-html-g5/charlie/",
+        "https://coda-school.github.io/25-html-g5/charlie/create_tl.html",
+        "https://coda-school.github.io/25-html-g5/charlie/search_tl.html",
+        "https://coda-school.github.io/25-html-g5/lucas/",
     ]
 };
