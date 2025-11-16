700cb6642976af87f62192c0d9282a3deec57a2d
Marc Bouvier <m.bouvier.dev@gmail.com>
2025-11-16 11:04:16 +0100

    chore: cleanup site data and results for template use

--- _data/sites/template.js
+++ _data/sites/template.js
@@ -1,12 +1,12 @@
 module.exports = {
 	name: "template",
-	description: "Modèle point de départ des projets",
+	description: "Ce site Speedlify",
     options: {
         runs: 1, // Run sample every time
         frequency: 60 * 24, // Every 1 day
         freshChrome: "site" // Use "site" if sites are all on the same origin and share assets.
     },
 	urls: [
-		"https://coda-school.github.io/template-projet-html-groupe/"
+		"https://baldir-fr.github.io/template-speedlify-gh-actions/"
 	]
 };
